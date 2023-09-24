c322468ce2506c4c39088de95e7cc0c32da07cdc
MShpiz <88736099+MShpiz@users.noreply.github.com>
2023-09-24 21:27:00 +0300

    Update program.asm

--- homework3/program.asm
+++ homework3/program.asm
@@ -1,25 +1,26 @@
 .data
 sep:    .asciz  "--------\n"    # Строка-разделитель (с \n и нулём в конце)
-prompt: .asciz  "n = ? "         # Подсказка для ввода числа
+prompt: .asciz  "how many elements are you going to summ? "         # Подсказка для ввода числа
 error:  .asciz  "incorrect n!\n"  # Сообщение о некорректном вводе
 overflow_error:  .asciz  "number overflow occured!\n"  # Сообщение о некорректном вводе
 correct_sum:	.asciz 	"sum: "
 last:	.asciz 	"last "
 element_count:	.asciz 	"elements summed: "
+even_elements:	.asciz	"number of even elements: "
+odd_elements:	.asciz	"number of odd elements: "
 ln:	.asciz 	"\n"
 .align  2                       # Выравнивание на границу слова
 n:	.word	0		# Число введенных элементов массива
 array:  .space  40              # 64 байта
-
 .text
 in:
         la 	a0, prompt      # Подсказка для ввода числа элементов массива
         li 	a7, 4           
         ecall
         
-        li      a7 5            # вводим количество элементов в массиве
+        li      a7 5            # Системный вызов №5 — ввести десятичное число
         ecall
-        mv      t3 a0           
+        mv      t3 a0           # Сохраняем результат в t3 (это n)
         ble     t3 zero fail    # На ошибку, если меньше 0
         
         li      t4 10           # Максимальный размер массива
@@ -50,6 +51,7 @@ fill:
         li	t6 0 		#сумма
         li	t5 0		# разница между суммой и границей
         li	t4 0		# счетчик просуммированных значений
+        li	t2 0		# счетчик четных элементов
         li	s1 2147483647   # максимальное положительное
         li	s2 -2147483648   # минимальное отрицательное
         
@@ -73,7 +75,7 @@ summ_proces:
         la 	a0, last       
         li 	a7, 4           
         ecall
-        jal	end
+        jal	even_count_process		# я не хочу делать спагетти-монстра из этого цикла поэтому четные числа считаем в отдельном цикле 
         
 	end_check:
 	
@@ -82,12 +84,24 @@ summ_proces:
 	addi    t0 t0 4         
         addi    t3 t3 -1        
         bnez    t3 summ_proces 
-        
+
+        lw	t3 n  
+        la      t0 array
+even_count_process:		# цикл подсчета четных чисел
+	lw	t1 (t0)
+	li	t5, 2
+	rem	t5, t1, t5		# в условии ничего не говорилось про то, что нельзя использовать команду взятия остатка от деления
+	bnez 	t5, cont
+	addi	t2, t2, 1
 	
-		 
-        
+	cont:
+	addi    t0 t0 4         
+        addi    t3 t3 -1        
+        bnez    t3 even_count_process 	 
+
+        lw	t3 n       
 end:
-	la 	a0, correct_sum       # выводим всю информацию про сумму
+	la 	a0, correct_sum       	# вывод всех данных
         li 	a7, 4           
         ecall
         mv	a0, t6
@@ -101,6 +115,27 @@ end:
         li 	a7, 4           
         ecall
         mv	a0, t4
+        li	a7, 1
+        ecall
+        la 	a0, ln       
+        li 	a7, 4           
+        ecall
+        
+        la 	a0, even_elements       
+        li 	a7, 4           
+        ecall
+        mv	a0, t2
+        li	a7, 1
+        ecall
+        la 	a0, ln       
+        li 	a7, 4           
+        ecall
+        
+        la 	a0, odd_elements       
+        li 	a7, 4           
+        ecall
+        sub	t2, t3, t2
+        mv	a0, t2
         li	a7, 1
         ecall
         la 	a0, ln       
